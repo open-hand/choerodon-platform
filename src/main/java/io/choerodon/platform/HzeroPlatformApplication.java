0feec977c627a0712b6e9e8f11eb6b094f8f6478
scp <changping.shi@hand-china.com>
2020-03-27 15:45:12 +0800

    [ADD] 添加文件

---
+++ src/main/java/io/choerodon/platform/HzeroPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.choerodon.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzeroPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzeroPlatformApplication.class, args);
+    }
+
+}
