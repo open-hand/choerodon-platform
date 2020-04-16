9c92879bfc3d0866d7a4c73dd5976e431c1248a1
lihao <310103270@qq.com>
2020-04-16 14:39:07 +0800

    [IMP] 更新pom.xml和.gitlab-ci.yml

--- src/main/java/io/choerodon/platform/PlatformApplication.java
+++ src/main/java/io/choerodon/platform/PlatformApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HzeroPlatformApplication {
+public class PlatformApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HzeroPlatformApplication.class, args);
+        SpringApplication.run(PlatformApplication.class, args);
     }
 
 }
